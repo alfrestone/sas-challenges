133ef8749a66657769d8bede89634f90cbb91334
alfredstone <zakariyya.azdadaille@gmail.com>
2026-09-01 10:53:58 +0100

    how let in js is being used for variable declaration

---
+++ Day0/let.js
@@ -0,0 +1,33 @@
+// Regular testing
+//-----------------------------------//
+let a = 5;
+a = "Hello";
+console.log(a);
+//-----------------------------------//
+let x = 5;
+let y = 6;
+console.log(x + y);
+//-----------------------------------//
+let FirstName = "Mahmod";
+let LastName = "Karim";
+console.log(FirstName + " " + LastName);
+//-----------------------------------//
+let $2Spacer$ = "Allen";
+let _$4Spacer$ = "Spacer";
+console.log($2Spacer$ + " " + _$4Spacer$);
+//-----------------------------------//
+let Num1 = 15;
+let Num2 = 14.1;
+console.log(Num1 - Num2);
+//-----------------------------------//
+let voider = null;
+let star = "42";
+let deter = voider - star;
+console.log(deter);
+console.log(typeof deter);
+//-----------------------------------//
+let value1 = true;
+let value2 = false;
+let text1 = "Hello";
+console.log(value1 + value1);
+console.log(value1 - text1);
