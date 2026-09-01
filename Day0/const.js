fa8c02ec02d0583552fc232cc769aef502bc85bc
alfredstone <zakariyya.azdadaille@gmail.com>
2026-09-01 10:55:13 +0100

    How const is used in javascript for variable declaration

---
+++ Day0/const.js
@@ -0,0 +1,32 @@
+// Regular testing
+//-----------------------------------//
+const a = 5;
+console.log(a);
+//-----------------------------------//
+const x = 5;
+const y = 6;
+console.log(x + y);
+//-----------------------------------//
+const FirstName = "Mahmod";
+const LastName = "Karim";
+console.log(FirstName + " " + LastName);
+//-----------------------------------//
+const $2Spacer$ = "Allen";
+const _$4Spacer$ = "Spacer";
+console.log($2Spacer$ + " " + _$4Spacer$);
+//-----------------------------------//
+const Num1 = 15;
+const Num2 = 14.1;
+console.log(Num1 - Num2);
+//-----------------------------------//
+const voider = null;
+const star = "42";
+const deter = voider - star;
+console.log(deter);
+console.log(typeof deter);
+//-----------------------------------//
+const value1 = true;
+const value2 = false;
+const text1 = "Hello";
+console.log(value1 + value1);
+console.log(value1 - text1);
